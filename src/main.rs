f8a520935df3e537640ff8b1670731ddaa34e637
DanielDubon <dub22233@uvg.edu.gt>
2024-08-03 00:14:26 -0600

    feat: 3D render added

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,32 @@ fn draw_cell(framebuffer: &mut Framebuffer, xo: usize, yo: usize, block_size:usi
 
 }
 
+fn render3d(framebuffer: &mut Framebuffer, player: &Player) {
+    let maze = load_maze("./maze.txt");
+    let block_size = 100;
+
+    let hh = framebuffer.height as f32 / 2.0;
+
+    let num_rays = framebuffer.width;
+    for i in 0..num_rays {
+        let current_ray = (i as f32 / num_rays as f32);
+        let a = player.a - (player.fov / 2.0) + (player.fov * current_ray);
+        let Intersect = cast_ray(framebuffer, &maze, player, a, block_size, false);
+        
+        let stake_height = (framebuffer.height as f32 / Intersect.distance) * 70.0;
+        
+        let stake_top = (hh - (stake_height / 2.0)) as usize;
+        let stake_bottom = (hh + (stake_height / 2.0)) as usize;
+
+        
+
+        for y in stake_top..stake_bottom {
+            framebuffer.set_current_color(0x33AADD);
+            framebuffer.point(i, y)
+        }
+    }
+
+}
 
 fn render2d(framebuffer: &mut Framebuffer, player: &Player) {
     let maze = load_maze("./maze.txt");
@@ -95,7 +121,7 @@ fn main() {
     fov: PI/3.0,
     };
 
-    
+    let mut mode = "3D";
     // initialize values
    
 
@@ -105,11 +131,17 @@ fn main() {
         if window.is_key_down(Key::Escape) {
             break;
         }
+        if window.is_key_down(Key::M){
+            mode = if mode == "2D" {"3D"} else {"2D"} 
+        }
         process_events(&window, &mut player); 
 
         framebuffer.clear();
-
-        render2d(&mut framebuffer, &player);
+        if mode == "2D"{
+            render2d(&mut framebuffer, &player);
+        }else{
+            render3d(&mut framebuffer, &player);
+        }
         // Update the window with the framebuffer contents
 window
 .update_with_buffer(&framebuffer.buffer, framebuffer_width, framebuffer_height)
--- src/player.rs
+++ src/player.rs
@@ -9,8 +9,8 @@ pub struct Player{
 }
 
 pub fn process_events(window: &Window, player: &mut Player) {
-    const MOVE_SPEED: f32 = 5.0;
-    const ROTATION_SPEED: f32 = 3.14 / 10.0;
+    const MOVE_SPEED: f32 = 4.0;
+    const ROTATION_SPEED: f32 = 3.14 / 40.0;
 
     if window.is_key_down(Key::Left) {
         player.a -= ROTATION_SPEED;
