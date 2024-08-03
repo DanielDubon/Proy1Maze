35c0e07edca3ecaed17a18239a82b06c02ff1ecd
Daniel <dub22233@uvg.edu.gt>
2024-08-02 18:58:55 -0600

    feat: maze is able to be rendered

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,47 @@ use std::f32::consts::PI;
 
 mod framebuffer;
 use framebuffer::Framebuffer;
+mod maze;
+use maze::load_maze;
+
+fn cell_to_color(cell: char) -> u32 {
+    match cell {
+        '+' => 0xFF00FF,
+        '-' => 0xDD11DD,
+        '|' => 0xCC11CC,
+        'g' => 0xFF0000,
+        _ => 0x000000,
+    }
+}
+
+
+fn draw_cell(framebuffer: &mut Framebuffer, xo: usize, yo: usize, block_size:usize, cell: char) {
+
+    for x in xo..xo + block_size{
+        for y in yo..yo + block_size{
+            if cell != ' '{
+            let color = cell_to_color(cell);
+            framebuffer.set_current_color(color);
+            framebuffer.point(x,y);
+            }
+        }
+    }
+
+
+}
+
+
+fn render2d(framebuffer: &mut Framebuffer) {
+    let maze = load_maze("./maze.txt");
+    let block_size = 100;
+
+    for row in 0..maze.len(){
+        for col in 0..maze[row].len(){
+            draw_cell(framebuffer, col * block_size, row * block_size,block_size, maze[row][col]);
+
+        }
+    }
+}
 
 
 fn main() {
@@ -28,8 +69,10 @@ fn main() {
     window.set_position(100, 100);
     window.update();
 
-    // initialize values
     framebuffer.set_background_color(0x333355);
+    framebuffer.clear();
+    // initialize values
+   
 
 
     while window.is_open() {
@@ -38,6 +81,7 @@ fn main() {
             break;
         }
 
+        render2d(&mut framebuffer);
         // Update the window with the framebuffer contents
 window
 .update_with_buffer(&framebuffer.buffer, framebuffer_width, framebuffer_height)
